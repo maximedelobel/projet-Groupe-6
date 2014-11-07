c8b8fac52bdfa601ad469cd62846ea6d33bd3855
delobel.maxime <delobel.maxime@INFO-128-10.iutcb.univ-littoral.fr>
2014-11-07 17:11:25 +0100

    projet chat fonction remove operationnelle

---
+++ petcatalog-master/src/main/java/controller/Catalog.java
@@ -0,0 +1,103 @@
+/*
+ * Catalog.java TEST2
+ *
+ */
+package controller;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import model.Item;
+
+/**
+ *
+ * @author carol mcdonald
+ */
+@ManagedBean
+@SessionScoped
+public class Catalog implements Serializable {
+
+    @EJB
+    private ItemFacade itemFacade;
+    private Item item = null;
+    private List<Item> items = null;
+    private PagingInfo pagingInfo = null;
+
+    /**
+     * Creates a new instance of Catalog
+     */
+    public Catalog() {
+        pagingInfo = new PagingInfo();
+    }
+
+    public PagingInfo getPagingInfo() {
+        if (pagingInfo.getItemCount() == -1) {
+            pagingInfo.setItemCount(getItemCount());
+        }
+        return pagingInfo;
+    }
+
+    public List<Item> getNextItems(int maxResults, int firstResult) {
+        return itemFacade.findRange(maxResults, firstResult);
+    }
+
+    public List<Item> getItems() {
+        if (items == null) {
+            getPagingInfo();
+            items = getNextItems(pagingInfo.getBatchSize(), pagingInfo.getFirstItem());
+        }
+        return items;
+    }
+
+    public Item getItem() {
+        return item;
+
+    }
+
+    public void addCat() {
+        Item it = new Item(String.valueOf(itemFacade.getItemCount()+1), "1", "Cat Women", "Cat test!", BigDecimal.valueOf(10.0));
+        itemFacade.create(it);
+    }
+    
+    public void remCat(Item it) {
+            itemFacade.remove(it);
+    }
+
+    public String next() {
+        reset(false);
+        getPagingInfo().nextPage();
+        return "list";
+    }
+
+    public String prev() {
+        reset(false);
+        getPagingInfo().previousPage();
+        return "list";
+    }
+
+    public String showDetail(Item item) {
+        this.item = item;
+        return "detail";
+    }
+    
+    public String showUpload(Item item) {
+        this.item = item;
+        return "upload";
+    }
+
+    public int getItemCount() {
+        return itemFacade.getItemCount();
+    }
+
+    private void reset(boolean resetFirstItem) {
+        item = null;
+        items = null;
+        pagingInfo.setItemCount(-1);
+        if (resetFirstItem) {
+            pagingInfo.setFirstItem(0);
+        }
+    }
+}
